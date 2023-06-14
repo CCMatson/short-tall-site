8b6a04945eb11bc5f56d8156e6121814906703d3
CCMatson <catherine.matson@gmail.com>
2023-06-14 15:49:14 -0700

    create mobile nav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
+import {useState} from 'react'
+import {AiOutlineMenu, AiOutlineHome, AiFillCrown, AiOutlineTrophy, AiTwotoneThunderbolt} from 'react-icons/ai'
 
 const SideNav = () => {
 const [nav, setNav] = useState(false)
@@ -11,6 +11,31 @@ const handleNav = () => {
   return (
     <div>
       <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      {
+        nav ? (
+          <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+            <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 curser-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineHome size={20}/>
+              <span className='pl-4'>Home</span>
+            </a>
+            <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 curser-pointer hover:scale-110 ease-in duration-200'>
+              <AiFillCrown size={20}/>
+              <span className='pl-4'>Performance</span>
+            </a>
+            <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 curser-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineTrophy size={20}/>
+              <span className='pl-4'>Contact</span>
+            </a>
+            <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 curser-pointer hover:scale-110 ease-in duration-200'>
+              <AiTwotoneThunderbolt size={20}/>
+              <span className='pl-4'>Socials</span>
+            </a>
+          </div>
+        )
+        : (
+          <div></div>
+        )
+      }
     </div>
   )
 }
