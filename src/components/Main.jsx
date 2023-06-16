45f5aa8e62092c7c1dbe88866fa8b7f0dcf170b0
CCMatson <catherine.matson@gmail.com>
2023-06-16 11:55:50 -0700

    add background image, create animated text header

---
+++ src/components/Main.jsx
@@ -0,0 +1,43 @@
+// import React from 'react'
+import { TypeAnimation } from "react-type-animation"
+import {FaInstagram} from 'react-icons/fa'
+
+const Main = () => {
+  return (
+    <div id='main' className="flex justify-center items-center h-screen" >
+      <img className='max-h-full' src="https://i.imgur.com/Pq4kG2a.jpg" />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/75">
+    <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+      <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">Short and Tall Productions</h1>
+      <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">We Are
+      <TypeAnimation
+      sequence={[
+        'Roxanne and Tony',
+        2000, 
+        'Producers',
+        2000,
+        'Drag Performers',
+        2000,
+        'Short and Tall',
+        2000,
+        'Rebel Rebels',
+        2000
+      ]}
+      wrapper="div"
+      cursor={true}
+      speed={50}
+      style={{ fontSize: '1em', paddingLeft: '5px', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+      </h2>
+      <div>
+          <FaInstagram className='cursor-pointer' size={20}/>
+      </div>
+    </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Main
